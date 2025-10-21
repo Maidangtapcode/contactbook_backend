ac0a9e6904e0b380e3dabd14746e676c54501f7a
Maidangtapcode <nhomai2408@gmail.com>
2025-10-21 23:27:55 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routers/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/",(req,res) => {
-    res.json({message: "Welcome to contact book application."});
-
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application." });
+});
+app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+app.user((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
 });
 module.exports = app;
