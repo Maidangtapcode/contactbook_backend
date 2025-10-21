be5688513a3ef8d8723d70895c265433fee821b0
Maidangtapcode <nhomai2408@gmail.com>
2025-10-22 00:48:01 +0700

    dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routers/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
 const app = express();
 app.use(cors());
@@ -12,9 +12,9 @@ app.use("/api/contacts", contactsRouter);
 app.use((req, res, next) => {
   return next(new ApiError(404, "Resource not found"));
 });
-app.user((err, req, res, next) => {
-  return res.status(error.statusCode || 500).json({
-    message: error.message || "Internal Server Error",
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
   });
 });
 module.exports = app;
